fee16784654e5b7295f97731390f8d8296c0ca5e
Chase Sillevis <chase@sillevis.net>
2012-04-20 21:41:36 +0200

    Init

---
+++ assets/js/script.js
@@ -0,0 +1,92 @@
+// The main URL (subject to change)
+var url = 'http://designers.sillevis.net/',
+    parent = $( '.designers ul' )
+
+$( window ).load( function() {
+	// Show a beatiful full background
+	$.backstretch( url + '/app/assets/img/bg.png' );
+	
+	// Only show everything when the JS has loaded
+	$( 'body' ).css( 'visibility', 'visible' )
+	
+	// Get all of the designers
+	$.post( url + 'nl/ajax', function(response) {
+		if( response.response ) {
+			$.each( response.persons, function() {
+				console.log( '+1 person' )		
+				console.log( this )		
+				$( '<li />' )
+					.attr( 'id', 'designer-' + this.id )
+					.append(
+						$( '<span />' )
+							.addClass( 'hover' )
+							.append(
+								$( '<strong />' ).text( this.name )
+							)
+							.append(
+								$( '<a />' )
+									.attr({
+										'href': this.url,
+										'title': this.url_title,
+										'target': '_blank'
+									})
+									.text( this.url )
+							)
+					)
+					.append(
+						$( '<a />' )
+							.addClass( 'avatar' )
+							.attr({
+								'href': this.url,
+								'title': this.name,
+								'target': '_blank'
+							})
+							.append(
+								$( '<img />' )
+									.attr({
+										'src': this.avatar[0]
+									})
+							)
+					)
+					.append(
+						$( '<span />' )
+							.addClass( 'twitter' )
+							.attr( 'data-user', this.twitter )
+							.append(
+								$( '<em />' )
+							)
+					)
+					.append(
+						$( '<span />' )
+							.addClass( 'dribbble' )
+							.attr( 'data-user', this.dribbble )
+							.append(
+								$( '<em />' )
+							)
+					)
+					.appendTo( parent )
+			})
+		} else {
+			alert( 'Something went wrong! Please try this app again later.' )
+		}
+	}, "json" ).complete( function() {
+		// Run this after getting the designers
+		$.each( $( '.designers ul li' ), function() {
+			$( this ).children( '.twitter, .dribbble' ).bind( 'click', function() {
+				var user = $( this ).attr( 'data-user' )
+			
+				if( $( this ).hasClass( 'twitter' ) )
+					setTimeout( function() { window.open( 'https://twitter.com/intent/user?screen_name=' + user ) }, 500 )
+					
+				if( $( this ).hasClass( 'dribbble' ) )
+					setTimeout( function() { window.open( 'http://dribbble.com/' + user ) }, 500 )
+					
+				if( $( this ).hasClass( 'enabled' ) ) {
+					$( this ).removeClass( 'enabled' )
+				} else {
+					$( this ).addClass( 'enabled' )
+				}
+			})
+		})
+	})
+})
